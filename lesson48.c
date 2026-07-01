ce0d92b1151da054171c415f9e214795d39d8b24
laur3312 <boboafriday@mail.ru>
2026-07-01 12:33:54 +0300

    Add files via upload

---
+++ lesson48.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+
+int main(void)
+{
+    int mainx = 100;
+
+    {
+        int inyx = 100;
+        printf("Inyx = %d\n", inyx);
+        printf("mainx = %d\n", mainx);
+
+    }
+    {
+    int tys = 200;
+    printf("Tys: %d\n",tys);
+
+    }
+    return 0;
+
+}
